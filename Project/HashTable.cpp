2509f1a585fec9daa943be650d1eb0d653a78346
bijan102 <bfazeli1@gmail.com>
2017-03-03 09:52:19 -0800

    Implemented overloaded <<, default const, and overloaded constructor.

--- Project/HashTable.cpp
+++ Project/HashTable.cpp
@@ -1,1 +1,29 @@
 #include "HashTable.h"
+
+ostream & operator<<(ostream & out, const HashTable & hashTable)
+{
+	// TODO: insert return statement here
+	for (int i = 0; i < hashTable.capacity; i++)
+		out << "Index " << i << " " << (hashTable.table[i] != -1) ? hashTable.table[i] : 'E';
+
+	return out;
+}
+
+HashTable::HashTable()
+{
+	capacity = CAPACITY;
+	numOfElements = 0;
+
+	table = new int[capacity];
+	fill_n(table, capacity, -1);	// Fills the array with -1's
+}
+
+HashTable::HashTable(int capacity)
+{
+	this->capacity = capacity;
+	numOfElements = 0;
+
+	table = new int[capacity];
+	fill_n(table, capacity, -1);
+}
+
